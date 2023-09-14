a454edd19462774fc02745591530fe513ed63086
Lucas Zorrer <lucaszorrer10@outlook.com>
2023-09-14 14:52:22 -0300

    Add token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,63 @@
 // controllers/userController.js
-const User = require('../models/user');
+const yup = require("yup");
+const bcrypt = require('bcrypt')
+const User = require("../models/user");
+const Op = require('Sequelize');
 
 // Create a new user
 const createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
+    const schema = yup.object().shape({
+      password: yup
+        .string("Enter a password.")
+        .required("Enter a password.")
+        .min(6, "Make a stronger password."),
+      email: yup
+        .string("Enter your email.")
+        .required("Enter your email.")
+        .email("Enter a valid email."),
+      name: yup.string("Enter your name.").required("Enter your name."),
+    });
+
+  
+
+    await schema.validate(req.body);
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+
+    const existingUser = await User.findOne({
+      where: {
+        name
+      },
+    });
+
+    if (existingUser) {
+      const errors = [];
+      if (existingUser.name === name) {
+        errors.push('Username already exists');
+      }
+      if (existingUser.email === email) {
+        errors.push('Email already exists');
+      }
+
+      return { success: false, message: errors.join(', ') };
+    }
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     res.status(201).json({
-      message: 'User created successfully',
+      message: "User created successfully",
       user,
     });
   } catch (error) {
-    console.error('Error creating user:', error);
+    console.error("Error creating user:", error);
     res.status(500).json({
-      message: 'Error creating user',
+      message: "Error creating user",
       error: error.message,
     });
   }
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require("express");
-const UserController = require("./controllers/userController");
 const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Hello, World!");
 });
 
-app.post("/signup", UserController.createUser);
+app.get("/home", authenticateToken, (req, res) => {
+  res.json("Hello, Home!");
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(4000, () => {
-  console.log("server is running.");
+  console.log("Main server is running.");
 });
